ee7b0fbef4a7a7a49c6cac2e680dc7d41d43160b
victorovejero <58417233+victorovejero@users.noreply.github.com>
2022-03-20 12:48:06 +0100

    Delete loadUsers.js

--- demo 2/src/main/resources/static/loadUsers.js
+++
@@ -1,9 +0,0 @@
-async function loadUsers(){
-    let response =  await fetch('./users.json');
-    console.log(response);
-    let users = response.json();
-    console.log(users.users);
-
-    return users;
-
-}
